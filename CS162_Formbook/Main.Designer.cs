7eed1d1d7761d1bbd4864dff7a8062242d1afb4b
DeveloperSpoot <52226518+DeveloperSpoot@users.noreply.github.com>
2024-05-20 14:43:29 -0700

    Add project files.

---
+++ CS162_Formbook/Main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CS162_Formbook
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.phoneListDataSet1 = new CS162_Formbook.PhoneListDataSet();
+            this.personBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.personTableAdapter1 = new CS162_Formbook.PhoneListDataSetTableAdapters.PersonTableAdapter();
+            this.tableAdapterManager1 = new CS162_Formbook.PhoneListDataSetTableAdapters.TableAdapterManager();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneListDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dataGridView2.DataSource = this.personBindingSource1;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(430, 392);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // phoneListDataSet1
+            // 
+            this.phoneListDataSet1.DataSetName = "PhoneListDataSet";
+            this.phoneListDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personBindingSource1
+            // 
+            this.personBindingSource1.DataMember = "Person";
+            this.personBindingSource1.DataSource = this.phoneListDataSet1;
+            // 
+            // personTableAdapter1
+            // 
+            this.personTableAdapter1.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager1
+            // 
+            this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager1.PersonTableAdapter = this.personTableAdapter1;
+            this.tableAdapterManager1.UpdateOrder = CS162_Formbook.PhoneListDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "PersonID";
+            this.dataGridViewTextBoxColumn1.HeaderText = "PersonID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Name";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Phone";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Phone";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(600, 416);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneListDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource phoneListDataSetBindingSource;
+        private PhoneListDataSet phoneListDataSet;
+        private System.Windows.Forms.BindingSource personBindingSource;
+        private PhoneListDataSetTableAdapters.PersonTableAdapter personTableAdapter;
+        private PhoneListDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private PhoneListDataSet phoneListDataSet1;
+        private System.Windows.Forms.BindingSource personBindingSource1;
+        private PhoneListDataSetTableAdapters.PersonTableAdapter personTableAdapter1;
+        private PhoneListDataSetTableAdapters.TableAdapterManager tableAdapterManager1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    }
+}
+
---
+++ CS162_Formbook/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS162_Formbook
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'phoneListDataSet1.Person' table. You can move, or remove it, as needed.
+            this.personTableAdapter1.Fill(this.phoneListDataSet1.Person);
+            // TODO: This line of code loads data into the 'phoneListDataSet.Person' table. You can move, or remove it, as needed.
+
+        }
+    }
+}
